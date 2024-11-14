0822c5e0059fa9e7a3ebb428748f7238f3abcf84
Robin Fantino <robin.fantino.rf@gmail.com>
2024-11-14 17:11:00 +0100

    j'ai terminer les template driven form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ArticlePageComponent } from './pages/article-page/article-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'article/:id', component: ArticlePageComponent },
+    { path: 'contact', component: ContactPageComponent },
     { path: '**', component: NotFoundPageComponent },
 ];
